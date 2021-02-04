5c3545c371cd2907f123115c6e9305935a949066
dn27 <dnewsom1@stevens.edu>
2021-02-04 17:06:15 -0500

    Question 1 Complete

--- CS546_Labs_DN27/Lab1/lab1.js
+++ CS546_Labs_DN27/Lab1/lab1.js
@@ -1,5 +1,36 @@
 const questionOne = function questionOne(arr) {
     // Implement question 1 here
+    // Function that returns true if a number is prime, false otherwise
+    function primeCheck(value) {
+        if(value === 1) {
+            return false;
+        }
+        else if (value === 2) {
+            return true;
+        }
+        else {
+            for(var num = 2; num < value; num++) {
+                if (value % num === 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    // Returns array of true/falses
+    let arrayCheck = arr.map(primeCheck);
+    // Empty object that must be populated
+    let primeObj = {};
+    // Take each number in the array,
+    arr.forEach((number) => {
+        // And take each boolean corresponding to number
+        arrayCheck.forEach((bool) => {
+            // Add number as key and bool as value into primeObj object
+            primeObj[number] = bool;
+        });
+      });
+
+    return primeObj;
     
 }
 
