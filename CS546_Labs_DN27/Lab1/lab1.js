33db6b0ae95b22c7bca4e974a7f5cd1c1268b96c
dn27 <dnewsom1@stevens.edu>
2021-02-04 16:23:56 -0500

    Labs

---
+++ CS546_Labs_DN27/Lab1/lab1.js
@@ -0,0 +1,26 @@
+const questionOne = function questionOne(arr) {
+    // Implement question 1 here
+    
+}
+
+const questionTwo = function questionTwo(arr) { 
+    // Implement question 2 here
+}
+
+const questionThree = function questionThree(text) {
+    // Implement question 3 here
+}
+
+const questionFour = function questionFour(num1, num2,num3) {
+    // Implement question 4 here
+}
+
+module.exports = {
+    firstName: "D'ANTOINE", 
+    lastName: "NEWSOME", 
+    studentId: "10456747",
+    questionOne,
+    questionTwo,
+    questionThree,
+    questionFour
+};
