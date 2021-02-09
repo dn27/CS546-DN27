8e21fd87676143a0db0e0e14518b6f82099318dd
dn27 <dnewsom1@stevens.edu>
2021-02-09 12:54:47 -0500

    Formatted and Prepped for Submission

--- CS546_Labs_DN27/Lab1/lab1.js
+++ CS546_Labs_DN27/Lab1/lab1.js
@@ -1,130 +1,133 @@
+/* 
+D'Antoine Newsome
+CS 546 WS - Lab 1
+I pledge my honor that I have abided by the Stevens Honor System */
 const questionOne = function questionOne(arr) {
-    // Implement question 1 here
-    // Function that returns true if a number is prime, false otherwise
-    function primeCheck(value) {
-        if (value === 1 || value <= 0) {
-            return false;
+  // Implement question 1 here
+  // Function that returns true if a number is prime, false otherwise
+  function primeCheck(value) {
+    if (value === 1 || value <= 0) {
+      return false;
+    } else if (value === 2) {
+      return true;
+    } else {
+      for (var num = 2; num < value; num++) {
+        if (value % num === 0) {
+          return false;
         }
-        else if (value === 2) {
-            return true;
-        } else {
-            for (var num = 2; num < value; num++) {
-                if (value % num === 0) {
-                    return false;
-                }
-             }
-             return true;
-        }
+      }
+      return true;
     }
+  }
 
-    if (arr == undefined) {
-        return {};
-    }
-    // Empty object that must be populated
-    let primeObj = {};
+  if (arr == undefined) {
+    return {};
+  }
+  // Empty object that must be populated
+  let primeObj = {};
 
-    // Goes through given array
-    arr.forEach((x) => {
-        // Check if prime or not
-        let isPrime = primeCheck(x);
-        // Adds key: x and value: is prime
-        primeObj[x] = isPrime;
-        
-    });
-    return primeObj;
-    
-}
+  // Goes through given array
+  arr.forEach((x) => {
+    // Check if prime or not
+    let isPrime = primeCheck(x);
+    // Adds key: x and value: is prime
+    primeObj[x] = isPrime;
+  });
+  return primeObj;
+};
 
-const questionTwo = function questionTwo(arr) { 
-    // Implement question 2 here
-    if (arr == undefined) {
-        return 0;
-    }
-    let sumOfSquares = arr.reduce((currentTotal, newValue) => {
-        let squared = newValue * newValue;
-        const newTotal = currentTotal + squared;
-        return newTotal;
-      }, 0);
-    
-    let sumRaised = Math.pow(sumOfSquares, 5);
-    let sqRoot = Math.sqrt(sumRaised);
-    sqRoot = Math.round(sqRoot * 100) / 100
-    return sqRoot;
-}
+const questionTwo = function questionTwo(arr) {
+  // Implement question 2 here
+  if (arr == undefined) {
+    return 0;
+  }
+  let sumOfSquares = arr.reduce((currentTotal, newValue) => {
+    let squared = newValue * newValue;
+    const newTotal = currentTotal + squared;
+    return newTotal;
+  }, 0);
+
+  let sumRaised = Math.pow(sumOfSquares, 5);
+  let sqRoot = Math.sqrt(sumRaised);
+  sqRoot = Math.round(sqRoot * 100) / 100;
+  return sqRoot;
+};
 
 const questionThree = function questionThree(text) {
-    // Implement question 3 here
-    const strCount = {
-        consonants: 0,
-        vowels: 0,
-        numbers: 0,
-        spaces: 0,
-        punctuation: 0,
-        specialCharacters: 0
-    };
-    
-    
-    const onlyCons = /[bcdfghjklmnpqrstvwxyz]/gi;
-    const onlyVowel = /[aeiou]/gi;
-    const onlyNumbers = /\d/g;
-    const whiteSpace = /\s/g;
-    const onlyPunc = /[.,?!'":;_(){}\[\]-]/g;
-    const specialChar = /[#\$%\^&@]/g;
+  // Implement question 3 here
+  const strCount = {
+    consonants: 0,
+    vowels: 0,
+    numbers: 0,
+    spaces: 0,
+    punctuation: 0,
+    specialCharacters: 0,
+  };
 
-    if (text === "" ) {
-        return strCount;
-    }
+  const onlyCons = /[bcdfghjklmnpqrstvwxyz]/gi;
+  const onlyVowel = /[aeiou]/gi;
+  const onlyNumbers = /\d/g;
+  const whiteSpace = /\s/g;
+  const onlyPunc = /[.,?!'":;_(){}\[\]-]/g;
+  const specialChar = /[#\$%\^&@]/g;
 
-    // If character is B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Z, Y, 
-    // add to consonant counter
-    let consMatch = (text.match(onlyCons) || []);
-    let consCount = consMatch.length;
-    strCount.consonants = consCount;
-    // If character is A, E, I, O, U, add to vowel counter
-    let vowelMatch = (text.match(onlyVowel) || []);
-    let vowelCount = vowelMatch.length;
-    strCount.vowels = vowelCount;
-    // If character is a space, add to space counter
-    let spaceMatch = (text.match(whiteSpace) || []);
-    let spaceCount = spaceMatch.length;
-    strCount.spaces = spaceCount;
-    // If character is a number, add to number counter
-    let numMatch = (text.match(onlyNumbers) || []);
-    let numCount = numMatch.length;
-    strCount.numbers = numCount;
-    // If character is a . , ? ! ' " : ; - ... _ () {} []
-    let puncMatch = (text.match(onlyPunc) || []);
-    let puncCount = puncMatch.length;
-    strCount.punctuation = puncCount;
-    // If character is a **#  $  % & ^ @ %**, add to special character counter
-    let characterMatch = (text.match(specialChar) || []);
-    let charCount = characterMatch.length;
-    strCount.specialCharacters = charCount;
-
-
-
+  if (text === "") {
     return strCount;
-}
+  }
+
+  // If character is B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Z, Y,
+  // add to consonant counter
+  let consMatch = text.match(onlyCons) || [];
+  let consCount = consMatch.length;
+  strCount.consonants = consCount;
+
+  // If character is A, E, I, O, U, add to vowel counter
+  let vowelMatch = text.match(onlyVowel) || [];
+  let vowelCount = vowelMatch.length;
+  strCount.vowels = vowelCount;
+
+  // If character is a space, add to space counter
+  let spaceMatch = text.match(whiteSpace) || [];
+  let spaceCount = spaceMatch.length;
+  strCount.spaces = spaceCount;
+
+  // If character is a number, add to number counter
+  let numMatch = text.match(onlyNumbers) || [];
+  let numCount = numMatch.length;
+  strCount.numbers = numCount;
+
+  // If character is a . , ? ! ' " : ; - ... _ () {} []
+  let puncMatch = text.match(onlyPunc) || [];
+  let puncCount = puncMatch.length;
+  strCount.punctuation = puncCount;
+
+  // If character is a **#  $  % & ^ @ %**, add to special character counter
+  let characterMatch = text.match(specialChar) || [];
+  let charCount = characterMatch.length;
+  strCount.specialCharacters = charCount;
+
+  return strCount;
+};
 
 const questionFour = function questionFour(num1, num2, num3) {
-    // Implement question 4 here
-    let loanAmt = num1;
-    // Convert interest percentage to decimal number by / by 100 and parseFloat
-    let interest = parseFloat(num2) / 100;
-    interest = interest / 12;
-    let term = num3 * 12;
-    let monthlyPayment = loanAmt * interest * Math.pow((1 + interest), term);
-    let mpDenom = Math.pow((1 + interest), term) - 1;
-    monthlyPayment = monthlyPayment / mpDenom;
-    return  Math.round(monthlyPayment * 100) / 100;
-}
+  // Implement question 4 here
+  let loanAmt = num1;
+  // Convert interest percentage to decimal number by / by 100 and parseFloat
+  let interest = parseFloat(num2) / 100;
+  interest = interest / 12;
+  let term = num3 * 12;
+  let monthlyPayment = loanAmt * interest * Math.pow(1 + interest, term);
+  let mpDenom = Math.pow(1 + interest, term) - 1;
+  monthlyPayment = monthlyPayment / mpDenom;
+  return Math.round(monthlyPayment * 100) / 100;
+};
 
 module.exports = {
-    firstName: "D'ANTOINE", 
-    lastName: "NEWSOME", 
-    studentId: "10456747",
-    questionOne,
-    questionTwo,
-    questionThree,
-    questionFour
+  firstName: "D'ANTOINE",
+  lastName: "NEWSOME",
+  studentId: "10456747",
+  questionOne,
+  questionTwo,
+  questionThree,
+  questionFour,
 };
--- CS546_Labs_DN27/Lab1/lab1.test.js
+++ CS546_Labs_DN27/Lab1/lab1.test.js
@@ -1,74 +1,67 @@
 const lab1 = require("./lab1");
 
-console.log(lab1.questionOne([1, 2, 3])); 
-// should return and output {'1': false, '2': true, '3': true}
+console.log(lab1.questionOne([25, 227, 39]));
+// should return and output { '25': false, '39': false, '227': true }
 
-console.log(lab1.questionOne([5, 3, 10])); 
-//returns and outputs: {'5':true, '3': true, '10': false} 
+console.log(lab1.questionOne([]));
+//returns and outputs: {}
 
-console.log(lab1.questionOne([2])); 
-// returns and outputs: {'2': true} 
+console.log(lab1.questionOne([2931]));
+// returns and outputs: { '2931': false }
 
-console.log(lab1.questionOne([5, 10, 9])); 
-//returns and outputs: {'5': true, '10': false, '9': false}
+console.log(lab1.questionOne([123, 124, 125, 126, 127]));
+//returns and outputs: { '123': false, '124': false, '125': false, '126': false, '127': true }
 
-console.log(lab1.questionOne([2, 7, 9, 1013])); 
-// returns and outputs: {'2': true, '7': true, '9': false, '1013': true}
+console.log(lab1.questionOne([99, 199, 301, 283, 5894, 239, 958]));
+// returns and outputs: {'99': false, '199': true, '239': true, '283': true, '301': false, '958': false, '5894': false}
 
-console.log(lab1.questionOne([])); 
-// returns and outputs: {}
+console.log(lab1.questionTwo([87, 88, 89, 90]));
+// should return and output 173795928366.6
 
-console.log(lab1.questionOne()); 
-// returns and outputs: {}
+console.log(lab1.questionTwo([1, 111, 1111]));
+//returns and outputs: 1735222845831347.8
 
-console.log(lab1.questionTwo([1,2,3])); 
-// should return and output 733.36 
+console.log(lab1.questionTwo([20, 2, 2]));
+// returns and outputs: 3362407.98
 
-console.log(lab1.questionTwo([5, 3, 10])); 
-//returns and outputs: 207855.73
+console.log(lab1.questionTwo([6, 11, 1]));
+//  returns and outputs: 313792.61
 
-console.log(lab1.questionTwo([1,2,3])); 
-// returns and outputs: 733.36 
-
-console.log(lab1.questionTwo([5, 10, 9])); 
-// returns and outputs: 609071.18
-
-console.log(lab1.questionTwo([2, 7, 9])); 
-//  returns and outputs: 207855.73
-
-console.log(lab1.questionTwo([])); 
+console.log(lab1.questionTwo([]));
 //returns and outputs: 0
 
-console.log(lab1.questionThree("The quick brown fox jumps over the lazy dog.")); 
-// should return and output {consonants: 24, vowels: 11, numbers: 0, spaces: 8, punctuation: 1, specialCharacters: 0}
+console.log(lab1.questionThree("How many shrimp do you have to eat"));
+// should return and output { consonants: 16, vowels: 11, numbers: 0, spaces: 7, punctuation: 0, specialCharacters: 0 }
 
-console.log(lab1.questionThree("How now brown cow!!!"));
-// returns and outputs: {consonants: 10, vowels: 4, numbers: 0, spaces: 3, punctuation: 3, specialCharacters: 0}
+console.log(lab1.questionThree("Performing their 2019 program, Medium,"));
+// returns and outputs: { consonants: 18, vowels: 10, numbers: 4, spaces: 4, punctuation: 2, specialCharacters: 0 }
 
-console.log(lab1.questionThree("One day, the kids from the neighborhood carried my mother's groceries all the way home. You know why? It was out of respect."));
-//  returns and outputs: {consonants: 61, vowels: 36, numbers: 0, spaces: 22, punctuation: 5, specialCharacters: 0}
+console.log(
+  lab1.questionThree(
+    "WGI Sport of the Arts is proud to present: Music City Mystique!"
+  )
+);
+//  returns and outputs: { consonants: 33, vowels: 17, numbers: 0, spaces: 11, punctuation: 2, specialCharacters: 0 }
 
-console.log(lab1.questionThree("CS 546 is going to be fun & I'm looking forward to working with you all this semester!!" )); 
-// returns and outputs: {consonants: 40, vowels: 23, numbers: 3, spaces: 17, punctuation: 3, specialCharacters: 1}
+console.log(
+  lab1.questionThree("#bossup #getthebread @breadwinners %100 all day erryday")
+);
+// returns and outputs: { consonants: 28, vowels: 14, numbers: 3, spaces: 6, punctuation: 0, specialCharacters: 4 }
 
-console.log(lab1.questionThree("")); 
-// returns and outputs: {consonants: 0, vowels: 0, numbers:0, spaces: 0, punctuation: 0, specialCharacters: 0}
+console.log(lab1.questionThree("?"));
+// returns and outputs: { consonants: 0, vowels: 0, numbers: 0, spaces: 0, punctuation: 1, specialCharacters: 0 }
 
-console.log(lab1.questionFour(25000, 3.11, 5)); 
-// should return and output: 450.44
+console.log(lab1.questionFour(60000, 3.5, 7));
+// should return and output: 806.39
 
-console.log(lab1.questionFour(25000, 3.11, 5)); 
-// Loan Amount: 25,000 , interest rate: 3.11% (0.0311), term: 5 years (5*12 = 60 monthly payments)
-//returns and outputs: 450.44
+console.log(lab1.questionFour(15600, 2.2, 4));
+//returns and outputs: 339.81
 
-console.log(lab1.questionFour(30000, 5, 6)); 
-//returns and outputs: 483.15
+console.log(lab1.questionFour(22300, 4, 8));
+//returns and outputs: 271.82
 
-console.log(lab1.questionFour(19500, 7, 3)); 
-//returns and outputs: 602.10
+console.log(lab1.questionFour(45000, 1.3, 9));
+//returns and outputs: 441.74
 
-console.log(lab1.questionFour(55000, 2, 6)); 
-//returns and outputs: 811.27
-
-console.log(lab1.questionFour(33000, 4.5, 2)); 
-//returns and outputs: 1440.38
+console.log(lab1.questionFour(43200, 5, 3));
+//returns and outputs: 1294.74
